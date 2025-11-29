c278e509a784c007ea536992cc9f0227d82737ef
Doug Chimento <dchimento@gmail.com>
2025-11-29 10:48:46 +0000

    fix: increase nonce amt

--- cli-tests/nonce.sh
+++ cli-tests/nonce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 name=$(basename "$0")
-cargo run -p solana-cli -- --config "${1:?}" create-nonce-account --verbose  ./cli-tests/"${name%.sh}".json 0.001
+cargo run -p solana-cli -- --config "${1:?}" create-nonce-account --verbose ./cli-tests/"${name%.sh}".json 0.1
